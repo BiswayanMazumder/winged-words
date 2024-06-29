ce37164e57b1dece66e953dc43c1d21b07b47c8e
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-06-29 12:50:45 +0530

    for you heading created

--- src/Components/posts.js
+++ src/Components/posts.js
@@ -1,24 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Posts() {
+  const selectedTab = (tab) => {
+    console.log(tab);
+  }
+
   return (
     <>
       <div className="posts">
         <div className="tablist">
-          <div className="foryou">
+          <div className="foryou" onClick={() => selectedTab('For you')}>
             For you
-            <div class="headingselected">
-              
-            </div>
+            <div className="headingselected"></div>
           </div>
-          <div className="following">
+          <div className="following" onClick={() => selectedTab('Following')}>
             Following
+            
           </div>
         </div>
         <div className="divider"></div>
       </div>
-      
-
     </>
-  )
+  );
 }
