d7a78a43369af289267fdfda8d3d8bffedf123f3
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-07-02 14:11:27 +0530

    Third section first part created

--- src/Components/homepagethirdsection.js
+++ src/Components/homepagethirdsection.js
@@ -6,6 +6,16 @@ export default function Homepagethirdsection() {
             <div className="container">
                 <div className="thidsection">
                     <input type="text" placeholder="Search" className='searchbar' />
+                    <div className="susbcribe">
+                        <h2>Subscribe to Premium</h2>
+                        <br></br>
+                        <p>Subscribe to unlock new features and if eligible, receive a share of ads revenue.</p>
+                        <div className="button1">
+                        <div className="postbutton">
+                            <div className="postbuttontext">Subscribe</div>
+                        </div>
+                    </div>
+                    </div>
                 </div>
             </div>
         </>
