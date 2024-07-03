336c40cefcddcef0cb69d44e98617abc0e922348
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-07-03 13:52:10 +0530

    cover pic and profile pic fetched

--- src/Components/accounttweets.jsx
+++ src/Components/accounttweets.jsx
@@ -51,7 +51,7 @@ export default function AccountTweets() {
         return null;
       }
     }));
-  
+
     const uniqueUserIds = [...new Set(tweets.map(tweet => tweet.owner))];
     const userDetails = await Promise.all(uniqueUserIds.map(async (userId) => {
       const userDetailsDocRef = doc(db, "User Details", userId);
@@ -70,24 +70,24 @@ export default function AccountTweets() {
         };
       }
     }));
-  
+
     const userMap = userDetails.reduce((acc, user) => {
       acc[user.id] = user;
       return acc;
     }, {});
-  
+
     setUsers(prevUsers => ({ ...prevUsers, ...userMap }));
-  
+
     const tweetData = tweets.map(tweet => ({
       ...tweet,
       name: userMap[tweet.owner]?.name,
       profilePic: userMap[tweet.owner]?.profilePic
     }));
-  
+
     setTweets(prevTweets => [...prevTweets, ...tweetData.filter(tweet => tweet !== null)]);
     setLastVisible(lastVisibleTweet + 10);
   };
-  
+
   const fetchtweets = async (lastVisibleTweet) => {
     setLoading(true);
     onAuthStateChanged(auth, async (user) => {
@@ -106,7 +106,7 @@ export default function AccountTweets() {
       setLoading(false);
     });
   };
-  
+
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -126,35 +126,105 @@ export default function AccountTweets() {
     console.log(tab);
   };
   const getusers = () => {
-      const app = initializeApp(firebaseConfig);
-      const analytics = getAnalytics(app);
-      const provider = new GoogleAuthProvider();
-      const auth = getAuth();
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-          const uid = user.uid;
-          // isloggedin = true;
-          console.log('signed in')
-          //   window.location.replace("loggedinpage.html")
-          // ...
-        } else {
-          // User is signed out
-          // ...
-          console.log('signed out')
-          window.location.replace("/")
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        // isloggedin = true;
+        console.log('signed in')
+        //   window.location.replace("loggedinpage.html")
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        console.log('signed out')
+        window.location.replace("/")
+      }
+    });
+
+  }
+  const [coverpic, setcoverpic] = useState('')
+  const [profilepic, setprofilepic] = useState([])
+  const getcoverpics = () => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDZ_ktB0uBgEPdU1tfaUfxWJ3sTqgEMmvs",
+      authDomain: "wingedwordsadmin.firebaseapp.com",
+      databaseURL: "https://wingedwordsadmin-default-rtdb.firebaseio.com",
+      projectId: "wingedwordsadmin",
+      storageBucket: "wingedwordsadmin.appspot.com",
+      messagingSenderId: "386908666811",
+      appId: "1:386908666811:web:a979774edcac6706c1229e",
+      measurementId: "G-38QRTWBK7L"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+    const analytics = getAnalytics(app);
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const uid = user.uid;
+        const userDocRef = doc(db, "Cover Picture", uid);
+        const docSnap = await getDoc(userDocRef);
+        if (docSnap.exists()) {
+          setcoverpic(docSnap.data()["Cover Picture"])
+          console.log("Cover Picture: ", coverpic);
         }
-      });
-    
+        
+      }
+    })
+  }
+  const getprofilepics = () => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDZ_ktB0uBgEPdU1tfaUfxWJ3sTqgEMmvs",
+      authDomain: "wingedwordsadmin.firebaseapp.com",
+      databaseURL: "https://wingedwordsadmin-default-rtdb.firebaseio.com",
+      projectId: "wingedwordsadmin",
+      storageBucket: "wingedwordsadmin.appspot.com",
+      messagingSenderId: "386908666811",
+      appId: "1:386908666811:web:a979774edcac6706c1229e",
+      measurementId: "G-38QRTWBK7L"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+    const analytics = getAnalytics(app);
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const uid = user.uid;
+        const userDocRef = doc(db, "User Details", uid);
+        const docSnap = await getDoc(userDocRef);
+        if (docSnap.exists()) {
+          setprofilepic(docSnap.data()["Profile Pic"])
+          console.log("profile Picture: ", profilepic);
+        }
+        
+      }
+    })
   }
   return (
     <>
-      <div className="posts" onLoad={getusers()}>
+      <div className="posts" onLoad={getusers()}{...getcoverpics()}{...getprofilepics()}>
         <div className="logomobile">
           <svg viewBox="0 0 24 24" width="30" height="30" aria-hidden="true" className="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-1nao33i r-rxcuwo r-1777fci r-m327ed r-494qqr">
             <g>
               <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" fill="white"></path>
             </g>
           </svg>
+        </div>
+        <div className="coverpicture">
+          <img src={coverpic} alt="" />
+        </div>
+        <div className="profilepic">
+          <img src={profilepic} alt="" />
         </div>
         <div className="tweets">
           {tweets.map((tweet, index) => (
