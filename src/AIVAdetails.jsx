e589242518c8a629450244d349726500593088bc
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-07-04 11:50:09 +0530

    docoment title changed

--- src/AIVAdetails.jsx
+++ src/AIVAdetails.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 export default function AIVAdetails() {
+    const getdocumentitle = () => {
+        document.title ='AIVA'
+    }
     return (
         <>
-            <div className="posts">
+            <div className="posts" onLoad={getdocumentitle()}>
                 <div className="videoplayer">
                     <video src="https://firebasestorage.googleapis.com/v0/b/wingedwordsadmin.appspot.com/o/aiva.mp4?alt=media&token=06d49060-e40e-411f-a10b-3a47ea8c2989" autoPlay muted loop></video>
                 </div>
