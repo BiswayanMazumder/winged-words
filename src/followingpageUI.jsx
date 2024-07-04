3cb4cdbf2f028291ac0fd9d390b3ca444fd0af41
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-07-04 15:28:40 +0530

    Following page designed

--- src/followingpageUI.jsx
+++ src/followingpageUI.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -6,16 +6,22 @@ import { getAuth, onAuthStateChanged, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
 import { useInView } from 'react-intersection-observer';
 import Homepagethirdsection from './Components/homepagethirdsection';
-export default function FollowingpageUI() {
-  const [tweets, setTweets] = useState([]);
-  const [users, setUsers] = useState({});
-  const [lastVisible, setLastVisible] = useState(null);
-  const [loading, setLoading] = useState(false);
 
+const FollowingpageUI = () => {
+  const [followers, setFollowers] = useState([]);
+  const [followerID, setFollowerID] = useState([]);
+  const [tweetid, setTweetid] = useState([]);
+  const [tweetbody, setTweetbody] = useState([]);
+  const [tweetimg, setTweetimg] = useState([]);
+  const [tweetusername, setTweetusername] = useState([]);
+  const [tweetuserphoto, setTweetuserphoto] = useState([]);
+  const [tweetownerid, settweetownerid] = useState([]);
+  const [loadingMore, setLoadingMore] = useState(false);
   const { ref, inView } = useInView({
     threshold: 0.5,
   });
 
+  // Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyDZ_ktB0uBgEPdU1tfaUfxWJ3sTqgEMmvs",
     authDomain: "wingedwordsadmin.firebaseapp.com",
@@ -30,143 +36,182 @@ export default function FollowingpageUI() {
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
-  const analytics = getAnalytics(app);
   const auth = getAuth();
   const provider = new GoogleAuthProvider();
-    const selectedTab = (tab) => {
-        console.log(tab);
-      };
-      const fetchtweets = async (lastVisibleTweet) => {
-    setLoading(true);
-    const userDocRef = doc(db, "Global Tweet IDs", "TIDs");
-    const docSnap = await getDoc(userDocRef);
-    if (docSnap.exists()) {
-      const tweetids = docSnap.data().TIDs;
 
-      const tweetsBatch = tweetids.slice(lastVisibleTweet, lastVisibleTweet + 10);
-      const tweets = await Promise.all(tweetsBatch.map(async (tweetId) => {
-        const tweetDocRef = doc(db, "Global Tweets", tweetId);
-        const tweetDocSnap = await getDoc(tweetDocRef);
-        if (tweetDocSnap.exists()) {
-          return {
-            id: tweetId,
-            body: tweetDocSnap.data()["Tweet Message"],
-            imageUrl: tweetDocSnap.data()["Image URL"],
-            owner: tweetDocSnap.data()["Uploaded UID"]
-          };
-        } else {
-          console.log("No such document!");
-          return null;
-        }
-      }));
-
-      const uniqueUserIds = [...new Set(tweets.map(tweet => tweet.owner))];
-      const userDetails = await Promise.all(uniqueUserIds.map(async (userId) => {
-        const userDetailsDocRef = doc(db, "User Details", userId);
-        const userDetailsDocSnap = await getDoc(userDetailsDocRef);
-        if (userDetailsDocSnap.exists()) {
-          return {
-            id: userId,
-            name: userDetailsDocSnap.data()["Name"],
-            profilePic: userDetailsDocSnap.data()["Profile Pic"] !== 'Placeholder' ? userDetailsDocSnap.data()["Profile Pic"] : 'https://images.pexels.com/photos/20419416/pexels-photo-20419416/free-photo-of-camera-in-red-background.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-          };
-        } else {
-          return {
-            id: userId,
-            name: "Unknown",
-            profilePic: 'https://images.pexels.com/photos/20419416/pexels-photo-20419416/free-photo-of-camera-in-red-background.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-          };
-        }
-      }));
-
-      const userMap = userDetails.reduce((acc, user) => {
-        acc[user.id] = user;
-        return acc;
-      }, {});
-
-      setUsers(prevUsers => ({ ...prevUsers, ...userMap }));
-
-      const tweetData = tweets.map(tweet => ({
-        ...tweet,
-        name: userMap[tweet.owner]?.name,
-        profilePic: userMap[tweet.owner]?.profilePic
-      }));
-
-      setTweets(prevTweets => [...prevTweets, ...tweetData.filter(tweet => tweet !== null)]);
-      setLastVisible(lastVisibleTweet + 10);
-      setLoading(false);
-    }
+  // Function to handle selected tab
+  const selectedTab = (tab) => {
+    console.log(tab);
   };
 
+  // Check user authentication state
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        fetchtweets(lastVisible);
-      }
-    });
-  }, []);
-
-  useEffect(() => {
-    if (inView && !loading) {
-      fetchtweets(lastVisible);
-    }
-  }, [inView]);
-  
-  const getusers = () => {
-      const app = initializeApp(firebaseConfig);
-      const analytics = getAnalytics(app);
-      const provider = new GoogleAuthProvider();
-      const auth = getAuth();
+    const checkAuthState = () => {
       onAuthStateChanged(auth, (user) => {
         if (user) {
-          const uid = user.uid;
-          // isloggedin = true;
-          console.log('signed in')
-          //   window.location.replace("loggedinpage.html")
-          // ...
+          console.log('User signed in');
         } else {
-          // User is signed out
-          // ...
-          console.log('signed out')
-          window.location.replace("/")
+          console.log('User signed out');
+          window.location.replace("/");
         }
       });
-    
-  }
-    return (
-        <>
-          <div className="posts">
-            <div className="tablist">
-              <Link  to="/home" className="following" onClick={() => selectedTab('For you')}>
-                For you
-                
-              </Link>
-              <div className="foryou" onClick={() => selectedTab('Following')}>
-                Following
-                <div className="headingselected"></div>
-              </div>
-            </div>
-            <div className="divider"></div>
-            <div className="tweets">
-          {tweets.map((tweet, index) => (
-            <div key={tweet.id} className="tweet" ref={index === tweets.length - 1 ? ref : null}>
-              <div className="userdetails">
+    };
+
+    checkAuthState();
+  }, [auth]);
+
+  // Fetch followers when component mounts or auth state changes
+  useEffect(() => {
+    const fetchFollowers = async () => {
+      const user = auth.currentUser;
+      if (user) {
+        const uid = user.uid;
+        const userDocRef = doc(db, "Following", uid);
+        try {
+          const docSnap = await getDoc(userDocRef);
+          if (docSnap.exists()) {
+            const followersData = docSnap.data()["Follower"];
+            setFollowers(followersData);
+            setFollowerID(followersData.map(follower => follower.ID));
+          }
+        } catch (error) {
+          console.error("Error fetching followers:", error);
+        }
+      }
+    };
+
+    fetchFollowers();
+  }, [auth, db]);
+
+  // Fetch tweet data for followers
+  useEffect(() => {
+    const fetchTweetData = async () => {
+      const user = auth.currentUser;
+      if (user && followerID.length > 0) {
+        try {
+          const promises = followerID.map(follower => {
+            const userDocRef = doc(db, "User Uploaded Tweet ID", follower);
+            return getDoc(userDocRef);
+          });
+
+          const snapshots = await Promise.all(promises);
+          const newTweetIds = [];
+          const newTweetBody = [];
+          const newTweetimg = [];
+          const newTweetownerid = [];
+          for (const docSnap of snapshots) {
+            if (docSnap.exists()) {
+              const data = docSnap.data();
+              newTweetIds.push(...data["TIDs"]);
+            }
+          }
+
+          // Limit the number of tweets fetched (example: first 10 tweets)
+          const limitedTweetIds = newTweetIds.slice(0, 10);
+
+          // Fetch tweet details using limited tweet IDs
+          const tweetPromises = limitedTweetIds.map(tweetId => {
+            const tweetDocRef = doc(db, "Global Tweets", tweetId);
+            return getDoc(tweetDocRef);
+          });
+
+          const tweetSnapshots = await Promise.all(tweetPromises);
+          for (const docSnap of tweetSnapshots) {
+            if (docSnap.exists()) {
+              const data = docSnap.data();
+              newTweetBody.push(data["Tweet Message"]);
+              newTweetimg.push(data["Image URL"]);
+              newTweetownerid.push(data["Uploaded UID"]);
+            }
+          }
+
+          setTweetid(limitedTweetIds);
+          setTweetbody(newTweetBody);
+          setTweetimg(newTweetimg);
+          settweetownerid(newTweetownerid);
+
+          // Fetch user details for tweet owners
+          const userPromises = newTweetownerid.map(ownerId => {
+            const userDocRef = doc(db, "User Details", ownerId);
+            return getDoc(userDocRef);
+          });
+
+          const userSnapshots = await Promise.all(userPromises);
+          const newTweetuserphoto = userSnapshots.map(docSnap => {
+            if (docSnap.exists()) {
+              return docSnap.data()["Profile Pic"];
+            }
+            return null;
+          });
+          const newTweetusername = userSnapshots.map(docSnap => {
+            if (docSnap.exists()) {
+              return docSnap.data()["Name"];
+            }
+            return null;
+          });
+          setTweetusername(newTweetusername);
+          setTweetuserphoto(newTweetuserphoto);
+        } catch (error) {
+          console.error("Error fetching tweets:", error);
+        }
+      }
+    };
+
+    fetchTweetData();
+  }, [auth, db, followerID]);
+
+  // Lazy loading additional tweets
+  useEffect(() => {
+    const fetchMoreTweets = async () => {
+      if (inView && !loadingMore) {
+        setLoadingMore(true);
+        // Simulate loading more tweets after a delay (example: 1 second)
+        setTimeout(() => {
+          setLoadingMore(false);
+          // Implement logic to fetch more tweets here if needed
+        }, 1000);
+      }
+    };
+
+    fetchMoreTweets();
+  }, [inView, loadingMore]);
+
+  // Render UI
+  return (
+    <>
+      <div className="posts">
+        <div className="tablist">
+          <Link to="/home" className="following" onClick={() => selectedTab('For you')}>
+            For you
+          </Link>
+          <div className="foryou" onClick={() => selectedTab('Following')}>
+            Following
+            <div className="headingselected"></div>
+          </div>
+        </div>
+        {/* <div className="divider"></div> */}
+        <br /><br />
+        <div className="tweets">
+          {tweetbody.map((tweet, index) => (
+            <div key={index} className="tweet" ref={index === tweetbody.length - 1 ? ref : null}>
+            <div className="userdetails">
                 <div className="profilepics">
-                  <img src={tweet.profilePic} alt="" className='profileimage' />
+                  <img src={tweetuserphoto[index]} alt="" className='profileimage' />
                   <div className="ownername">
-                    <p>{tweet.name}</p>
+                    <p>{tweetusername[index]}</p>
                   </div>
                 </div>
               </div>
-              <p>{tweet.body}</p>
-              <br></br>
-              {tweet.imageUrl && <img src={tweet.imageUrl} alt="Tweet Image" className='tweetimages' />}
+              <p>{tweet}</p>
+              <img src={tweetimg[index]} alt="" className='tweetimages' />
             </div>
           ))}
-          {loading && <p>Loading Wings...</p>}
+          {loadingMore && <p>Loading more...</p>}
         </div>
-          </div>
-          <Homepagethirdsection />
-        </>
-      );
-}
+      </div>
+      <Homepagethirdsection />
+    </>
+  );
+};
+
+export default FollowingpageUI;
