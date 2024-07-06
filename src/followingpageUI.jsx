4986f250f61c54d2cca175d44f1f6a4f6300dc77
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-07-06 12:20:19 +0530

    logo added for mobile

--- src/followingpageUI.jsx
+++ src/followingpageUI.jsx
@@ -17,7 +17,7 @@ const FollowingpageUI = () => {
   const [tweetuserphoto, setTweetuserphoto] = useState([]);
   const [tweetownerid, settweetownerid] = useState([]);
   const [loadingMore, setLoadingMore] = useState(false);
-  const [userid,setuserid] = useState([]);
+  const [userid, setuserid] = useState([]);
   const { ref, inView } = useInView({
     threshold: 0.5,
   });
@@ -130,7 +130,7 @@ const FollowingpageUI = () => {
           setTweetbody(newTweetBody);
           setTweetimg(newTweetimg);
           settweetownerid(newTweetownerid);
-          const UserID=[];
+          const UserID = [];
           // Fetch user details for tweet owners
           const userPromises = newTweetownerid.map(ownerId => {
             const userDocRef = doc(db, "User Details", ownerId);
@@ -184,6 +184,13 @@ const FollowingpageUI = () => {
   return (
     <>
       <div className="posts">
+        <div className="logomobile">
+          <svg viewBox="0 0 24 24" width="30" height="30" aria-hidden="true" className="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-1nao33i r-rxcuwo r-1777fci r-m327ed r-494qqr">
+            <g>
+              <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" fill="white"></path>
+            </g>
+          </svg>
+        </div>
         <div className="tablist">
           <Link to="/home" className="following" onClick={() => selectedTab('For you')}>
             For you
@@ -198,15 +205,15 @@ const FollowingpageUI = () => {
         <div className="tweets">
           {tweetbody.map((tweet, index) => (
             <div key={index} className="tweet" ref={index === tweetbody.length - 1 ? ref : null}>
-            <div className="userdetails">
+              <div className="userdetails">
                 <div className="profilepics">
                   <img src={tweetuserphoto[index]} alt="" className='profileimage' />
                   <div className="ownername">
-                    <Link className='ownertweetname' onClick={()=>{
-                      localStorage.setItem('userid',userid[index]);
-                      window.location.href='/other'
+                    <Link className='ownertweetname' onClick={() => {
+                      localStorage.setItem('userid', userid[index]);
+                      window.location.href = '/other'
                     }}>
-                    <p>{tweetusername[index]}</p>
+                      <p>{tweetusername[index]}</p>
                     </Link>
                     <div className="verified">
                       <svg viewBox="0 0 22 22" height='20' width='20' aria-label="Verified account" role="img" class="r-4qtqp9 r-yyyyoo r-1xvli5t r-bnwqim r-lrvibr r-m6rgpd r-f9ja8p r-og9te1" data-testid="icon-verified"><g><linearGradient gradientUnits="userSpaceOnUse" id="34-a" x1="4.411" x2="18.083" y1="2.495" y2="21.508"><stop offset="0" stop-color="#f4e72a"></stop><stop offset=".539" stop-color="#cd8105"></stop><stop offset=".68" stop-color="#cb7b00"></stop><stop offset="1" stop-color="#f4ec26"></stop><stop offset="1" stop-color="#f4e72a"></stop></linearGradient><linearGradient gradientUnits="userSpaceOnUse" id="34-b" x1="5.355" x2="16.361" y1="3.395" y2="19.133"><stop offset="0" stop-color="#f9e87f"></stop><stop offset=".406" stop-color="#e2b719"></stop><stop offset=".989" stop-color="#e2b719"></stop></linearGradient><g clip-rule="evenodd" fill-rule="evenodd"><path d="M13.324 3.848L11 1.6 8.676 3.848l-3.201-.453-.559 3.184L2.06 8.095 3.48 11l-1.42 2.904 2.856 1.516.559 3.184 3.201-.452L11 20.4l2.324-2.248 3.201.452.559-3.184 2.856-1.516L18.52 11l1.42-2.905-2.856-1.516-.559-3.184zm-7.09 7.575l3.428 3.428 5.683-6.206-1.347-1.247-4.4 4.795-2.072-2.072z" fill="url(#34-a)"></path><path d="M13.101 4.533L11 2.5 8.899 4.533l-2.895-.41-.505 2.88-2.583 1.37L4.2 11l-1.284 2.627 2.583 1.37.505 2.88 2.895-.41L11 19.5l2.101-2.033 2.895.41.505-2.88 2.583-1.37L17.8 11l1.284-2.627-2.583-1.37-.505-2.88zm-6.868 6.89l3.429 3.428 5.683-6.206-1.347-1.247-4.4 4.795-2.072-2.072z" fill="url(#34-b)"></path><path d="M6.233 11.423l3.429 3.428 5.65-6.17.038-.033-.005 1.398-5.683 6.206-3.429-3.429-.003-1.405.005.003z" fill="#d18800"></path></g></g></svg>
@@ -215,7 +222,7 @@ const FollowingpageUI = () => {
                 </div>
               </div>
               <p>{tweet}</p>
-              <br/>
+              <br />
               <img src={tweetimg[index]} alt="" className='tweetimages' />
             </div>
           ))}
