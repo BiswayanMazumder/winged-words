8f10c0f22fd2cc2c1e654276d9b76187bfa2f5b1
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-07-17 23:33:20 +0530

    premium subs status fetching

--- src/AIVAdetails.jsx
+++ src/AIVAdetails.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState, useCallback } from 'react';
+import { Link } from 'react-router-dom';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, onAuthStateChanged, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+const firebaseConfig = {
+    apiKey: "AIzaSyDZ_ktB0uBgEPdU1tfaUfxWJ3sTqgEMmvs",
+    authDomain: "wingedwordsadmin.firebaseapp.com",
+    databaseURL: "https://wingedwordsadmin-default-rtdb.firebaseio.com",
+    projectId: "wingedwordsadmin",
+    storageBucket: "wingedwordsadmin.appspot.com",
+    messagingSenderId: "386908666811",
+    appId: "1:386908666811:web:a979774edcac6706c1229e",
+    measurementId: "G-38QRTWBK7L"
+  };
 
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const auth = getAuth();
 export default function AIVAdetails() {
     const getdocumentitle = () => {
         document.title ='AIVA'
+    }
+    const provider = new GoogleAuthProvider();
+    const [subs,setsubs]=useState(false)
+    const getsusbstatus = async() => {
+        const user = auth.currentUser;
+        if (user) {
+            const uid = user.uid;
+            const userDocRef = doc(db, "Premium Subscribers", uid);
+            try {
+              const docSnap = await getDoc(userDocRef);
+              if (docSnap.exists()) {
+                const followersData = docSnap.data()["Premium"];
+                setsubs(followersData);
+              }else{
+                setsubs(false);
+              }
+            } catch (error) {
+              console.error("Error fetching followers:", error);
+            }
+          }
+        //   console.log('prem',subs)
     }
     return (
         <>
@@ -16,11 +56,13 @@ export default function AIVAdetails() {
 
                     </h1>
                 </div>
-                <div className="aivadetails">
+                <Link className="aivadetails" onClick={getsusbstatus}>
+                <div >
                     <div className="aivasubs">
                         <div className="postbuttontext">Subscribe</div>
                     </div>
                 </div>
+                </Link>
             </div>
         </>
     )
--- src/followingpageUI.jsx
+++ src/followingpageUI.jsx
@@ -163,7 +163,7 @@ const FollowingpageUI = () => {
             return getDoc(userDocRef);
           });
           setuserid(UserID);
-          console.log('users'+UserID);
+          // console.log('users'+UserID);
           const userSnapshots = await Promise.all(userPromises);
           const newTweetuserphoto = userSnapshots.map(docSnap => {
             if (docSnap.exists()) {
