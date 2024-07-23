ddae0c94491d1b2818d23eb59615754a1d5f1a56
BiswayanMazumder <biswayanmazumder27@gmail.com>
2024-07-23 22:51:59 +0530

    subscription status change

--- src/Components/homepagethirdsection.jsx
+++ src/Components/homepagethirdsection.jsx
@@ -1,6 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, onAuthStateChanged, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { useInView } from 'react-intersection-observer';
 
 export default function Homepagethirdsection() {
+    const [subscribed, setsubscribed] = useState(false);
+    const firebaseConfig = {
+      apiKey: "AIzaSyDZ_ktB0uBgEPdU1tfaUfxWJ3sTqgEMmvs",
+      authDomain: "wingedwordsadmin.firebaseapp.com",
+      databaseURL: "https://wingedwordsadmin-default-rtdb.firebaseio.com",
+      projectId: "wingedwordsadmin",
+      storageBucket: "wingedwordsadmin.appspot.com",
+      messagingSenderId: "386908666811",
+      appId: "1:386908666811:web:a979774edcac6706c1229e",
+      measurementId: "G-38QRTWBK7L"
+    };
+  
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+    const analytics = getAnalytics(app);
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    useEffect(() => {
+        const getsubscription =(() => {
+            onAuthStateChanged(auth, async (user) => {
+                if (user) {
+                    const uid = user.uid;
+                    const userDocRef = doc(db, "Premium Subscribers", uid);
+                    const docSnap = await getDoc(userDocRef);
+                    if (docSnap.exists()) {
+                        setsubscribed(true);
+                    }
+                    else{
+                        setsubscribed(false);
+                    }
+                }
+            })
+            console.log('Subscribed',subscribed)
+        })
+        getsubscription()
+    })
     return (
         <>
             <div className="container">
@@ -12,7 +55,7 @@ export default function Homepagethirdsection() {
                         <p>Subscribe to unlock new features and if eligible, receive a share of ads revenue.</p>
                         <div className="button1">
                         <div className="postbutton">
-                            <div className="postbuttontext">Subscribe</div>
+                            <div className="postbuttontext">{subscribed ? "Subscribed" : "Subscribe"}</div>
                         </div>
                     </div>
                     </div>
